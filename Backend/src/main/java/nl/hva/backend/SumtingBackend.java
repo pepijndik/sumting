09624fad5e6459186d06b5131f838af190331d16
pepijn <pepijn@pdik.nl>
2022-09-28 16:17:21 +0200

    Fixed Docker build process

--- Backend/src/main/java/nl/hva/backend/SumtingBackend.java
+++ Backend/src/main/java/nl/hva/backend/SumtingBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class SumtingBackend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(SumtingBackend.class, args);
 	}
 
 }
---
+++ Backend/src/main/java/nl/hva/backend/rest/HelpController.java
@@ -0,0 +1,12 @@
+package nl.hva.backend.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelpController {
+    @GetMapping("/help")
+    public String getAllOffers() {
+        return "Welcome to the Suming Rest api";
+    }
+}
