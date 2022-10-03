ae6f7095f9c8d6640ebd91378a41e38cc602e8f9
pepijn <pepijn@pdik.nl>
2022-10-03 13:18:05 +0200

    Sprint banner :-)

--- Backend/src/main/java/nl/hva/backend/SumtingBackend.java
+++ Backend/src/main/java/nl/hva/backend/SumtingBackend.java
@@ -1,14 +1,22 @@
 package nl.hva.backend;
 
+import nl.hva.backend.Server.CustomBanner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class SumtingBackend {
-	public static void main(String[] args) {
-		SpringApplication.run(SumtingBackend.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SumtingBackend.class);
+        application.setBanner(new CustomBanner());
+        application.run(args);
+    }
 }
