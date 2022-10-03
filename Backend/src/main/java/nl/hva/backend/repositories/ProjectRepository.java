8562d78476a7ed6bd27c04571f76b992d6645c96
pepijn <pepijn@pdik.nl>
2022-10-03 14:19:22 +0200

    Backend fixes

--- Backend/src/main/java/nl/hva/backend/repositories/ProjectRepository.java
+++ Backend/src/main/java/nl/hva/backend/repositories/ProjectRepository.java
@@ -3,8 +3,34 @@ package nl.hva.backend.repositories;
 import nl.hva.backend.models.Project;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProjectRepository extends EntityRepository<Project>
-{
+import java.util.List;
 
+@Repository
+public class ProjectRepository implements EntityRepository<Project> {
+
+
+    @Override
+    public Project save(Project entity) {
+        return null;
+    }
+
+    @Override
+    public Project findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Project update(Project entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(Project entity) {
+
+    }
+
+    @Override
+    public List<Project> findAll() {
+        return null;
+    }
 }
