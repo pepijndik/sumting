b40d28191dc249169d01b14bf7b802e896a2747c
Kaan <kaan.ugur@hva.nl>
2022-10-24 21:33:30 +0200

    chart added to dashboard and map structure changed

---
+++ frontend/src/Components/Chart/BarGraph.vue
@@ -0,0 +1,54 @@
+<template>
+  <Bar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :width="150"
+      :height="50"
+  />
+</template>
+
+<script>
+import {Bar} from 'vue-chartjs'
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: "BarGraph",
+  components: {
+    Bar
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            label: 'Data Bar',
+            data: [40, 20, 12, 39, 10, 40],
+            backgroundColor: '#E56B6F',
+            pointBackgroundColor: 'blue',
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+      }
+    }
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+  },
+  metaInfo: {
+    title: 'Dashboard',
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/Components/Chart/LineGraph.vue
@@ -0,0 +1,71 @@
+<template>
+  <Line
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :width="150"
+      :height="50"
+  />
+</template>
+
+<script>
+import {Line} from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale,
+} from 'chart.js'
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+)
+
+export default {
+  name: "LineGraph",
+  components: {
+    Line
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            label: 'Data Line',
+            data: [40, 20, 12, 39, 10, 40],
+            borderColor: '#E56B6F',
+            backgroundColor: '#E56B6F',
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+      }
+    }
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'line-chart'
+    }
+  },
+  metaInfo: {
+    title: 'Dashboard',
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
