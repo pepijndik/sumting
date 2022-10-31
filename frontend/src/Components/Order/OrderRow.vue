2351455d02ec85a22285fd7c53d85d777f13585f
Fenne <fennehattink@gmail.com>
2022-10-31 22:16:11 +0100

    Added delete button and order row component

---
+++ frontend/src/Components/Order/OrderRow.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="text-gray-300 dark:border-gray-700 bg-white font-normal w-full h-10 flex items-center pl-10 text-sm border-gray-300 rounded border shadow font-inter"
+  >
+    <img
+      :src="require(`@/Assets/img/icons/file.svg`)"
+      alt="File icon"
+      width="24"
+      class="ml-2 mr-2 rounded-md"
+    />
+    <p class="font-inter text-yInMnBlue font-bold pr-4">ID {{ order.id }}</p>
+    <div class="divider" />
+    <div class="pl-10 pr-10 h-10 items-center justify-between flex">
+      <p class="font-inter text-yInMnBlue font-bold pr-4">Total amount:</p>
+      <p class="font-inter text-candyPink font-bold">
+        € {{ order.transactionTotal }}
+      </p>
+    </div>
+    <div class="divider" />
+    <div class="float-right pl-4 h-10 items-center justify-between flex">
+      <p class="font-inter text-yInMnBlue font-bold pr-4">Date:</p>
+      <p class="font-inter text-candyPink font-bold">{{ order.order_date }}</p>
+    </div>
+    <div class="divider" />
+    <button class="float-center pl-4 h-10 items-center justify-between flex">
+      <img
+        :src="require(`@/Assets/img/icons/delete.svg`)"
+        alt="Close icon"
+        width="32"
+        class="ml-2 mr-2 bg-candyPink rounded-md"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderSubItem",
+  props: ["order"],
+};
+</script>
+
+<style scoped>
+.divider {
+  /*border-left: 2px solid rgb(55 65 81);*/
+  border-left: 1px solid rgb(209 213 219);
+  height: 26px;
+  margin: 0 10px;
+}
+</style>
--- frontend/src/Pages/Order/OrderView.vue
+++ frontend/src/Pages/Order/OrderView.vue
@@ -3,57 +3,73 @@
     <div>
       <p class="font-inter text-yInMnBlue">Client</p>
       <SearchableDropDown
-          class="mt-1"
-          :options="users"
-          :fields="['email','user_name']"
-          :primary-key="'id'"
-          placeholder="Choose a client">
+        class="mt-1"
+        :options="users"
+        :fields="['email', 'user_name']"
+        :primary-key="'id'"
+        placeholder="Choose a client"
+      >
       </SearchableDropDown>
     </div>
     <div class="mt-3 sm:mt-0">
       <p class="font-inter text-yInMnBlue">Project(s)</p>
       <SearchableDropDown
-          @selected="selectedProject= $event"
-          :options="projects"
-          :fields="['name']"
-          :primary-key="'id'"
-          :disabled="false"
-          autocomplete="off"
-          :maxItem="10"
-          class="mt-1"
-          placeholder="Select project(s)">
+        @selected="selectedProject = $event"
+        :options="projects"
+        :fields="['name']"
+        :primary-key="'id'"
+        :disabled="false"
+        autocomplete="off"
+        :maxItem="10"
+        class="mt-1"
+        placeholder="Select project(s)"
+      >
       </SearchableDropDown>
     </div>
   </div>
-  <div class="flex h-80 pl-4 pt-4 mt-4 text-sm border-gray-300 rounded border shadow">
-    <p class="font-Alatsi text-gray-300 text-base">No client selected.</p>
+  <div
+    class="h-80 pl-4 pt-4 mt-4 text-sm border-gray-300 rounded border shadow items-center justify-center"
+  >
+    <!--    <p v-if="this.orders" class="font-Alatsi text-gray-300 text-base">No client selected.</p>-->
+    <div
+      class="w-full h-10 items-center pl-10 text-sm justify-between"
+      v-for="item in computedObj"
+      :key="item.id"
+    >
+      <OrderRow :order="item" />
+    </div>
   </div>
 </template>
 
 <script>
 import SearchableDropDown from "@/Components/Form/SearchableDropDown";
+import OrderRow from "@/Components/Order/OrderRow.vue";
 
 export default {
   name: "OrderView",
-  components: {SearchableDropDown},
-  inject: ['ProjectApi','OrderApi','UserApi'],
+  components: { SearchableDropDown, OrderRow },
+  inject: ["ProjectApi", "OrderApi", "UserApi"],
   data() {
     return {
       projects: [],
       orders: [],
-      users:[],
+      users: [],
       selectedProject: null,
+      limit: 5,
     };
   },
   async created() {
     this.projects = await this.ProjectApi.SearchableDropDown();
     this.orders = await this.OrderApi.findAll();
-    this.users = await  this.UserApi.findAll();
+    //this.users = await  this.UserApi.findAll();
   },
-
-}
+  computed: {
+    computedObj() {
+      return this.limit ? this.orders.slice(0, this.limit) : this.orders;
+    },
+  },
+  methods() {},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
