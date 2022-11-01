93d1e152c761aba100538a41fc4e31a79a2352eb
Fenne <fennehattink@gmail.com>
2022-11-01 13:05:39 +0100

    fixed mobile and tablet view

--- frontend/src/Components/Order/OrderRow.vue
+++ frontend/src/Components/Order/OrderRow.vue
@@ -1,59 +1,59 @@
 <template>
   <div
-    class="text-gray-300 dark:border-gray-700 bg-white w-full h-10 flex text-sm border-gray-300 border-b mt-4 font-Alatsi"
+    class="dark:border-gray-700 w-full lg:h-10 md:h-20 flex text-sm border-gray-300 border-b mb-5 mt-5 font-Alatsi items-center pt-2 pb-2"
   >
-    <div class="float-left items-left flex pb-3 w-[250px]">
-      <img
-        :src="require(`@/Assets/img/icons/file.svg`)"
-        alt="File icon"
-        width="24"
-        class="ml-2 mr-2 rounded-md pb-2"
-      />
-      <p class="text-yInMnBlue pr-4">ID {{ order.id }}</p>
-    </div>
-
-    <div class="divider" />
-
-    <div class="h-10 pb-3 items-center flex w-[200px]">
-      <p class="text-yInMnBlue">Total amount:&nbsp;</p>
-      <p class="text-candyPink">€ {{ order.transactionTotal }}</p>
-    </div>
-
-    <div class="divider" />
-
-    <div class="h-10 pb-3 items-center flex w-[150px] font-Alatsi">
-      <p class="text-yInMnBlue">Date:&nbsp;</p>
-      <p class="text-candyPink">{{ orderDate }}</p>
-    </div>
-
-    <div class="divider" />
-
-    <button class="float-center items-center justify-between flex pb-4">
+    <div class="m-2 flex lg:flex-row flex-wrap border-r-2 pbb-2">
       <div
-        class="flex bg-yInMnBlue rounded-md m-4 lg:w-[150px] md:w-[100px] justify-center h-[32px]"
+        class="float-left items-left flex w-[80px] md:w-[100px] lg:w-[200px] items-center md:border-r-2 pr-1 mr-1"
       >
         <img
-          :src="require(`@/Assets/img/icons/edit.svg`)"
-          alt="Edit icon"
+          :src="require(`@/Assets/img/icons/file.svg`)"
+          alt="File icon"
           width="24"
-          class="ml-2 mr-2"
         />
+        <p class="text-yInMnBlue">ID {{ order.id }}</p>
       </div>
+
+      <!-- <div class="divider" /> -->
+      <div class="flex flex-row">
+        <div
+          class="h-5 lg:h-10 items-center flex lg:w-[200px] md:w-[180px] w-[120px] text-xs md:text-sm border-r-2 pr-1 mr-1"
+        >
+          <p class="text-yInMnBlue">Total amount:&nbsp;</p>
+          <p class="text-candyPink">€ {{ order.transactionTotal }}</p>
+        </div>
+
+        <!-- <div class="divider" /> -->
+
+        <div
+          class="h-5 lg:h-10 items-center flex lg:w-[150px] md:w-[100px] w-[85px] font-Alatsi text-xs md:text-sm"
+        >
+          <p class="text-yInMnBlue">Date:&nbsp;</p>
+          <p class="text-candyPink">{{ orderDate }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="divider" /> -->
+
+    <button
+      class="items-center flex bg-yInMnBlue rounded-md lg:w-[150px] md:w-[100px] w-[50px] justify-center h-[32px] mr-2 md:mr-4"
+    >
+      <img
+        :src="require(`@/Assets/img/icons/edit.svg`)"
+        alt="Edit icon"
+        width="24"
+      />
     </button>
     <button
       @click="delOrder()"
-      class="float-center items-center justify-between flex pb-4"
+      class="items-center flex bg-candyPink rounded-md lg:w-[150px] md:w-[100px] w-[50px] justify-center h-[32px]"
     >
-      <div
-        class="flex bg-candyPink rounded-md m-4 lg:w-[150px] md:w-[100px] justify-center h-[32px]"
-      >
-        <img
-          :src="require(`@/Assets/img/icons/delete.svg`)"
-          alt="Delete icon"
-          width="24"
-          class="ml-2 mr-2"
-        />
-      </div>
+      <img
+        :src="require(`@/Assets/img/icons/delete.svg`)"
+        alt="Delete icon"
+        width="24"
+      />
     </button>
   </div>
 </template>
@@ -101,7 +101,7 @@ export default {
 <style scoped>
 .divider {
   /*border-left: 2px solid rgb(55 65 81);*/
-  border-left: 1px solid rgb(209 213 219);
+  border-right: 1px solid rgb(209 213 219);
   height: 26px;
   margin: 0 10px;
 }
