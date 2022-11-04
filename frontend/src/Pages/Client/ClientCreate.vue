65539f07f863b097253b972e84040d432c40f6ff
Fenne <fennehattink@gmail.com>
2022-11-04 14:34:17 +0100

    created client pages

---
+++ frontend/src/Pages/Client/ClientCreate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p class="font-inter text-yInMnBlue">Client</p>
+
+    <button
+      class="my-2 w-full sm:w-80 bg-candyPink transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white font-inter px-8 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+    >
+      Create order
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "clientCreate",
+  components: {},
+  inject: ["UserApi"],
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async created() {},
+};
+</script>
+
+<style scoped></style>
--- frontend/src/Router/router.js
+++ frontend/src/Router/router.js
@@ -1,88 +1,115 @@
 // router/axios.ts
-import {createRouter, createWebHistory} from "vue-router";
-import Dashboard from "@/Pages/Dashboard";
 import Login from "@/Pages/Auth/Login";
-import ListProjects from "@/Pages/Projects/ListProjects";
+import ClientCreate from "@/Pages/Client/ClientCreate";
+import Dashboard from "@/Pages/Dashboard";
 import OrderCreate from "@/Pages/Order/OrderCreate";
 import OrderView from "@/Pages/Order/OrderView";
-import AuthenticationService from "@/Services/AuthenticationService";
-const IsAuthenticated = (to, from, next) => {
 
-    if (to.name !== 'auth:login' && !AuthenticationService.isLoggedIn()) next({ name: 'auth:login' })
-    else next()
-}
+import ListProjects from "@/Pages/Projects/ListProjects";
+import AuthenticationService from "@/Services/AuthenticationService";
+import { createRouter, createWebHistory } from "vue-router";
+const IsAuthenticated = (to, from, next) => {
+  if (to.name !== "auth:login" && !AuthenticationService.isLoggedIn())
+    next({ name: "auth:login" });
+  else next();
+};
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/login',
-            name: 'auth:login',
-            component: Login,
-            meta: {
-                layout: 'AuthLayout', // we add new meta layout here to use it later,
-                title: 'Login',
-            },
-        },
-        {
-            path: '/sign-up',
-            name: 'auth:sign_up',
-            meta: {
-                layout: 'AuthLayout', // same here
-                title: 'Sign up',
-            },
-        },
-        {
-            path: '/',
-            component: Dashboard,
-            name: 'dashboard',
-            beforeEnter: IsAuthenticated,
-            meta: {
-                layout: 'AdminLayout', // Not needed, but you can add it here DashboardLayout will automatically be used
-                pageTitle: 'Dashboard',
-                title: 'Graphs & Charts',
-                subtitle: 'Statistics'
-            },
-        },
-        {
-            path: '/orders',
-            beforeEnter: IsAuthenticated,
-            name: 'admin:Order',
-            meta: {
-                pageTitle: 'Orders',
-                title: 'Client order',
-                subtitle: 'View'
-            },
-            component: OrderView
-        },
-        {
-            path: '/orders/create',
-            name: 'admin:OrderCreate',
-            beforeEnter: IsAuthenticated,
-            meta: {
-                pageTitle: 'Create order',
-                title: 'Client order',
-                subtitle: 'Create'
-            },
-            component: OrderCreate
-        },
-        {
-            path: '/projects',
-            name: 'admin:projects',
-            beforeEnter: IsAuthenticated,
-            meta: {
-                pageTitle: 'Projects',
-                title: 'Project',
-                subtitle: 'View'
-            },
-            component: ListProjects
-        }
-    ]
-})
-
-router.beforeEach((to, from, next) => {
-    window.document.title = to.meta && to.meta?.pageTitle   ?  `${to.meta.pageTitle} | Sumting` : 'Sumting';
-    next();
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/login",
+      name: "auth:login",
+      component: Login,
+      meta: {
+        layout: "AuthLayout", // we add new meta layout here to use it later,
+        title: "Login",
+      },
+    },
+    {
+      path: "/sign-up",
+      name: "auth:sign_up",
+      meta: {
+        layout: "AuthLayout", // same here
+        title: "Sign up",
+      },
+    },
+    {
+      path: "/",
+      component: Dashboard,
+      name: "dashboard",
+      beforeEnter: IsAuthenticated,
+      meta: {
+        layout: "AdminLayout", // Not needed, but you can add it here DashboardLayout will automatically be used
+        pageTitle: "Dashboard",
+        title: "Graphs & Charts",
+        subtitle: "Statistics",
+      },
+    },
+    {
+      path: "/orders",
+      beforeEnter: IsAuthenticated,
+      name: "admin:Order",
+      meta: {
+        pageTitle: "Orders",
+        title: "Client order",
+        subtitle: "View",
+      },
+      component: OrderView,
+    },
+    {
+      path: "/orders/create",
+      name: "admin:OrderCreate",
+      beforeEnter: IsAuthenticated,
+      meta: {
+        pageTitle: "Create order",
+        title: "Client order",
+        subtitle: "Create",
+      },
+      component: OrderCreate,
+    },
+    {
+      path: "/projects",
+      name: "admin:projects",
+      beforeEnter: IsAuthenticated,
+      meta: {
+        pageTitle: "Projects",
+        title: "Project",
+        subtitle: "View",
+      },
+      component: ListProjects,
+    },
+    {
+      path: "/clients",
+      beforeEnter: IsAuthenticated,
+      name: "admin:Client",
+      meta: {
+        pageTitle: "Clients",
+        title: "Clients",
+        subtitle: "View",
+      },
+      component: OrderView,
+    },
+    {
+      path: "/clients/create",
+      name: "admin:ClientCreate",
+      beforeEnter: IsAuthenticated,
+      meta: {
+        pageTitle: "Create client",
+        title: "New client",
+        subtitle: "Create",
+      },
+      component: ClientCreate,
+    },
+  ],
 });
 
-export default router
+router.beforeEach((to, from, next) => {
+  window.document.title =
+    to.meta && to.meta?.pageTitle
+      ? `${to.meta.pageTitle} | Sumting`
+      : "Sumting";
+  next();
+});
+
+export default router;
