58f07202f05ee462e76448aae92fd1f4fc10608b
Cln89 <colin.laan@gmail.com>
2022-11-16 20:39:10 +0100

    Fix styling issues

--- frontend/src/Components/Form/SubItems/OrderSubItem.vue
+++ frontend/src/Components/Form/SubItems/OrderSubItem.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="focus:outline-none focus:ring-2 focus:ring-offset-2 md-flex-col flex-row
-            focus:ring-candyPink dark:border-gray-700 bg-white font-normal w-full md-h-12 h-20 flex
-            sm-text-sm text-xs border-gray-300 rounded border shadow font-inter justify-between text-left">
-    <div class="w-1/3 flex items-center h-20 md-h-10 pl-4 overflow-hidden">
-      <p class="text-yInMnBlue font-bold pr-4 ">{{ product.project.description }}</p>
+  <div class="focus:outline-none focus:ring-2 focus:ring-offset-2 flex-row flex
+            focus:ring-candyPink dark:border-gray-700 bg-white font-normal w-full md:h-12 h-16
+            sm:text-sm text-xs border-gray-300 rounded border shadow font-inter justify-between text-left">
+    <div class="w-1/3 flex items-center md:pl-4 pl-2 overflow-hidden">
+      <p class="text-yInMnBlue font-bold md:pr-4 pr-2">{{ product.project.description }}</p>
     </div>
     <div class="items-center flex">
       <div class="divider"/>
     </div>
-    <div class="w-1/3 flex items-center justify-center md-flex-row flex-col">
+    <div class="w-1/3 flex items-center justify-center md:flex-row flex-col pr-2 pl-2">
       <div class="flex-row flex items-center">
         <p class="text-candyPink font-bold">€{{ product.price }}</p>
         <p class="text-yInMrBlue font-bold p-1">/</p>
@@ -18,16 +18,16 @@
     <div class="items-center flex">
       <div class="divider"/>
     </div>
-    <div class="w-1/3 flex items-center">
-      <div class="md-flex-row flex-col">
-        <label for="amountToOrder" class="font-inter text-yInMnBlue font-bold p-4 flex">Amount</label>
+    <div class="w-1/3 flex items-center md:flex-row flex-row">
+      <div class="md:flex-row flex-col flex items-center pl-2 md:pl-0">
+        <label for="amountToOrder" class="font-inter text-yInMnBlue font-bold md:p-4 flex items-center">Amount</label>
         <InputComponentNumeric :inputData="amount" @update="amount = $event" id="amount" placeholder="Amount"
                                required autocomplete="" name="amount" type="number" @keyup="emitUpdate"
-                               :class="'rounded-md border font-inter text-yInMnBlue font-bold md-w-1/2 w-full h-8 place-self-center'"
+                               :class="'rounded-md border font-inter text-yInMnBlue font-bold md:w-1/2 w-full h-8'"
                                ref="amountToOrder"/>
       </div>
       <img :src="require(`@/Assets/img/icons/close.svg`)" alt="Close icon" width="36"
-           class="ml-2 mr-2 bg-candyPink rounded-md" @click="this.$emit('deleteProduct', this.index)">
+           class="ml-2 mr-2 bg-candyPink rounded-md md:w-8 md:h-8 h-6 w-6" @click="this.$emit('deleteProduct', this.index)">
     </div>
   </div>
 </template>
--- frontend/src/Components/Form/SubItems/OrderTotalCostSubItem.vue
+++ frontend/src/Components/Form/SubItems/OrderTotalCostSubItem.vue
@@ -20,9 +20,7 @@ export default {
   props: {
     products: {
       type: Array,
-      default: () => [{
-
-      }],
+      default: () => [{}],
       required: true,
     },
   },
