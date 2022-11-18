db6ab341448b6719ae4c18de6705ebab1c6acf72
Fenne <fennehattink@gmail.com>
2022-11-18 11:25:59 +0100

    kinda fixed double records in search dropdown

--- frontend/src/Components/Form/SearchableDropdownUser.vue
+++ frontend/src/Components/Form/SearchableDropdownUser.vue
@@ -4,49 +4,61 @@
 
     <div class="flex flex-col lg:mr-16">
       <div class="relative">
-        <div class="absolute text-gray-300 flex items-center pl-4 h-full cursor-pointer">
-          <svg class="fill-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 487.95 487.95" width="17"
-               height="17" xml:space="preserve">
-          <g>
+        <div
+          class="absolute text-gray-300 flex items-center pl-4 h-full cursor-pointer"
+        >
+          <svg
+            class="fill-gray-300"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 487.95 487.95"
+            width="17"
+            height="17"
+            xml:space="preserve"
+          >
             <g>
-              <path d="M481.8,453l-140-140.1c27.6-33.1,44.2-75.4,44.2-121.6C386,85.9,299.5,0.2,193.1,0.2S0,86,0,191.4s86.5,191.1,192.9,191.1
+              <g>
+                <path
+                  d="M481.8,453l-140-140.1c27.6-33.1,44.2-75.4,44.2-121.6C386,85.9,299.5,0.2,193.1,0.2S0,86,0,191.4s86.5,191.1,192.9,191.1
                 c45.2,0,86.8-15.5,119.8-41.4l140.5,140.5c8.2,8.2,20.4,8.2,28.6,0C490,473.4,490,461.2,481.8,453z M41,191.4
-                c0-82.8,68.2-150.1,151.9-150.1s151.9,67.3,151.9,150.1s-68.2,150.1-151.9,150.1S41,274.1,41,191.4z"/>
+                c0-82.8,68.2-150.1,151.9-150.1s151.9,67.3,151.9,150.1s-68.2,150.1-151.9,150.1S41,274.1,41,191.4z"
+                />
+              </g>
             </g>
-          </g>
           </svg>
         </div>
         <input
-            class="text-yInMnBlue focus:outline-none dark:border-gray-700 bg-white font-normal w-full sm:w-80 h-10 flex focus:border-candyPink  focus:border-0 focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-b-1
-            items-center pl-10 text-sm border-gray-300 rounded-t-md border shadow font-inter"
-            :name="name"
-            @focus="showOptions()"
-            @blur="exit()"
-            @keyup="keyMonitor"
-            v-model="searchFilter"
-            :disabled="disabled"
-            :placeholder="placeholder"
+          class="text-yInMnBlue focus:outline-none dark:border-gray-700 bg-white font-normal w-full sm:w-80 h-10 flex focus:border-candyPink focus:border-0 focus:border-t-2 focus:border-l-2 focus:border-r-2 focus:border-b-1 items-center pl-10 text-sm border-gray-300 rounded-t-md border shadow font-inter"
+          :name="name"
+          @focus="showOptions()"
+          @blur="exit()"
+          @keyup="keyMonitor"
+          v-model="searchFilter"
+          :disabled="disabled"
+          :placeholder="placeholder"
         />
       </div>
     </div>
-    <div class="dropdown-one w-full sm:w-80 rounded-b-md outline-none bg-white relative mt-0 shadow-md">
+    <div
+      class="dropdown-one w-full sm:w-80 rounded-b-md outline-none bg-white relative mt-0 shadow-md"
+    >
       <!-- Dropdown content -->
       <div
-          class="rounded w-full px-3 py-2 absolute top-1 right-0 bg-white shadow-lg z-10 overflow-y-scroll max-h-32 border-candyPink  border-0 border-b-2 border-l-2 border-r-2"
-          v-show="optionsShown">
+        class="rounded w-full px-3 py-2 absolute top-1 right-0 bg-white shadow-lg z-10 overflow-y-scroll max-h-32 border-candyPink border-0 border-b-2 border-l-2 border-r-2"
+        v-show="optionsShown"
+      >
         <div
-            class="flex items-center justify-between hover:bg-gray-100 rounded text-gray-600 hover:text-gray-800 p-3
-            hover:font-bold hover:cursor-default z-10"
-            @mousedown="selectOption(option)"
-            v-for="(option, index) in filteredOptions"
-            :key="index">
+          class="flex items-center justify-between hover:bg-gray-100 rounded text-gray-600 hover:text-gray-800 p-3 hover:font-bold hover:cursor-default z-10"
+          @mousedown="selectOption(option)"
+          v-for="(option, index) in filteredOptions"
+          :key="index"
+        >
           <div class="flex">
             <slot class="">
-<!--              <user-icon v-if="option.user_type = PERSON "/>-->
-<!--              <user-icon v-if="option.user_type = undefined"/>-->
-              <company-icon v-if="option.user_type = BUSINESS"/>
-<!--              <company-icon/>-->
-              <user-icon/>
+              <!--              <user-icon v-if="option.user_type = PERSON "/>-->
+              <!--              <user-icon v-if="option.user_type = undefined"/>-->
+              <!-- <company-icon v-if="option.user_type = BUSINESS"/> -->
+              <company-icon />
+              <user-icon />
             </slot>
             <p class="">{{ this.populatefields(option) }}</p>
           </div>
@@ -54,108 +66,115 @@
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
 import userIcon from "@/Components/SvgIcons/userIcon";
 import CompanyIcon from "@/Components/SvgIcons/CompanyIcon";
 
-
 export default {
-  name: 'SearchableDropDown',
-  components: {userIcon, CompanyIcon},
-  template: 'Dropdown',
+  name: "SearchableDropDown",
+  components: { userIcon, CompanyIcon },
+  template: "Dropdown",
 
   props: {
     name: {
       type: String,
       required: false,
-      default: 'dropdown',
-      note: 'Input name'
+      default: "dropdown",
+      note: "Input name",
     },
     options: {
       type: Array,
       required: true,
       default: Array,
-      note: 'Options of dropdown. An array of options with id and name',
+      note: "Options of dropdown. An array of options with id and name",
     },
     fields: {
       type: Array,
       required: false,
       // eslint-disable-next-line vue/require-valid-default-prop
-      default: ['description'],
-      note: 'Possible fields options'
+      default: ["description"],
+      note: "Possible fields options",
     },
     primaryKey: {
       type: String,
       required: false,
-      default: 'id',
-      note: 'Primary key of the object'
+      default: "id",
+      note: "Primary key of the object",
     },
     placeholder: {
       type: String,
       required: false,
-      default: 'Please select an option',
-      note: 'Placeholder of dropdown'
+      default: "Please select an option",
+      note: "Placeholder of dropdown",
     },
     disabled: {
       type: Boolean,
       required: false,
       default: false,
-      note: 'Disable the dropdown'
+      note: "Disable the dropdown",
     },
     maxItem: {
       type: Number,
       required: false,
       default: 6,
-      note: 'Max items showing'
-    }
+      note: "Max items showing",
+    },
   },
   data() {
     return {
       selected: {},
       optionsShown: false,
-      searchFilter: ''
-    }
+      searchFilter: "",
+    };
   },
   created() {
-    this.$emit('selected', this.selected);
+    this.$emit("selected", this.selected);
+  },
+  updated() {
+    console.log(this.options);
   },
   computed: {
     filteredOptions() {
       const filtered = [];
-      const regOption = new RegExp(this.searchFilter, 'ig');
-      for (const option of this.options) {
-        //Double For loop to find the option in the fields
-        this.fields.forEach(field => {
+      const regOption = new RegExp(this.searchFilter, "ig");
+
+      this.fields.forEach((field) => {
+        this.options.forEach((option) => {
           if (this.searchFilter.length < 1 || option[field].match(regOption)) {
             if (filtered.length < this.maxItem) filtered.push(option);
           }
         });
+      });
 
-      }
-
+      // for (const option of this.options) {
+      //   //Double For loop to find the option in the fields
+      //   this.fields.forEach((field) => {
+      //     if (this.searchFilter.length < 1 || option[field].match(regOption)) {
+      //       if (filtered.length < this.maxItem) filtered.push(option);
+      //     }
+      //   });
+      // }
+      console.log(filtered);
       return filtered;
     },
   },
   methods: {
     populatefields(option) {
       var finalString = "";
-      this.fields.forEach(field => {
-        finalString += this.extractFieldValue(option,field);
+      this.fields.forEach((field) => {
+        finalString += this.extractFieldValue(option, field);
         //Check if not the last field then append space with separator
-        if(this.fields.indexOf(field) !== this.fields.length -1)
-        {
-          finalString += " | "
+        if (this.fields.indexOf(field) !== this.fields.length - 1) {
+          finalString += " | ";
         }
       });
-      return finalString
+      return finalString;
     },
-    extractFieldValue(option,prop) {
+    extractFieldValue(option, prop) {
       // eslint-disable-next-line no-prototype-builtins
-      if(Object.hasOwn(option,prop))
-      {
+      if (Object.hasOwn(option, prop)) {
         return option[prop];
       }
     },
@@ -164,29 +183,29 @@ export default {
       this.optionsShown = false;
       console.log(this.selected[this.fields[0]]);
       this.searchFilter = this.selected[this.fields[0]]; //Set the search filter to the first field
-      this.$emit('selected', this.selected);
+      this.$emit("selected", this.selected);
     },
     showOptions() {
       if (!this.disabled) {
-        this.searchFilter = '';
+        this.searchFilter = "";
         this.optionsShown = true;
       }
     },
     exit() {
       if (!this.selected[this.primaryKey]) {
         this.selected = {};
-        this.searchFilter = '';
+        this.searchFilter = "";
       } else {
         this.searchFilter = this.selected[this.fields[0]];
       }
-      this.$emit('selected', this.selected);
+      this.$emit("selected", this.selected);
       this.optionsShown = false;
     },
     // Selecting when pressing Enter
     keyMonitor: function (event) {
       if (event.key === "Enter" && this.filteredOptions[0])
         this.selectOption(this.filteredOptions[0]);
-    }
+    },
   },
   watch: {
     searchFilter() {
@@ -195,13 +214,10 @@ export default {
       } else {
         this.selected = this.filteredOptions[0];
       }
-      this.$emit('filter', this.searchFilter);
-    }
-  }
+      this.$emit("filter", this.searchFilter);
+    },
+  },
 };
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/Pages/Order/OrderCreate.vue
+++ frontend/src/Pages/Order/OrderCreate.vue
@@ -2,55 +2,74 @@
   <div>
     <p class="font-inter text-yInMnBlue">Client</p>
     <SearchableDropdownUser
-        class="mt-1"
-        placeholder="Choose a client"
-        :options="users"
-        :fields="['name', 'email']"
-        :primary-key="'id'"
-        :disabled="false"
-        :max-item="50">
+      class="mt-1"
+      placeholder="Choose a client"
+      :options="users"
+      :fields="['name', 'email']"
+      :primary-key="'id'"
+      :disabled="false"
+      :max-item="10"
+    >
     </SearchableDropdownUser>
     <h3 class="font-inter text-2xl text-yInMnBlue font-bold">Order info</h3>
     <p class="font-inter text-yInMnBlue">Description</p>
-    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
 
     <p class="font-inter text-yInMnBlue mt-2">Project(s)</p>
-    <OrderSubItem name="Plant Trees in Africa" price_per="0,00" target_item="Tree" amount="0"/>
+    <OrderSubItem
+      name="Plant Trees in Africa"
+      price_per="0,00"
+      target_item="Tree"
+      amount="0"
+    />
 
-    <button class="my-2 w-full sm:w-80 bg-candyPink transition duration-150 ease-in-out hover:bg-indigo-600 rounded
-    text-white font-inter px-8 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
-      Create order</button>
+    <button
+      class="my-2 w-full sm:w-80 bg-candyPink transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white font-inter px-8 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+    >
+      Create order
+    </button>
   </div>
 </template>
 
 <script>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import OrderSubItem from "@/Components/Form/SubItems/OrderSubItem";
 import SearchableDropdownUser from "@/Components/Form/SearchableDropdownUser";
 export default {
   name: "OrderCreate",
-  components: {SearchableDropdownUser,OrderSubItem},
-  inject: ['ProjectApi', 'UserApi'],
+  components: { SearchableDropdownUser, OrderSubItem },
+  inject: ["ProjectApi", "UserApi"],
   data() {
     return {
       editor: ClassicEditor,
-      editorData: '<p>Sumting project description</p>',
+      editorData: "<p>Sumting project description</p>",
       editorConfig: {
-        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ]
+        toolbar: [
+          "heading",
+          "|",
+          "bold",
+          "italic",
+          "link",
+          "bulletedList",
+          "numberedList",
+          "blockQuote",
+        ],
       },
-      users: []
-    }
+      users: [],
+    };
   },
   async created() {
     this.users = await this.UserApi.GetAllUsers();
-    console.log(this.users)
+    //console.log(this.users);
   },
-}
+};
 </script>
 
 <style scoped>
-
-.mt-1  {
-
+.mt-1 {
 }
 </style>
