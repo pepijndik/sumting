c6610e3d6aa85e699de20859d3afc7d78e8d0f95
Cln89 <colin.laan@gmail.com>
2022-11-21 15:14:18 +0100

    AAAAAA

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@ import OrderApiService from "@/Services/Order/OrderApiService";
 import ProjectApiService from "@/Services/Projects/ProjectApiService";
 import AuthenticationService from "@/Services/AuthenticationService";
 import UserApiService from "@/Services/User/UserApiService";
-import DashboardApiService from "@/Services/Dashboard/DashboardApiService";
+import ProductApiService from "@/Services/Products/ProductApiService";
 export default {
   name: 'App',
   components: {
@@ -22,7 +22,7 @@ export default {
       Auth: new AuthenticationService(),
       ProjectApi: new ProjectApiService(),
       UserApi: new UserApiService(),
-      DashboardApi: new DashboardApiService(),
+      ProductApi: new ProductApiService()
     }
   },
   data() {
