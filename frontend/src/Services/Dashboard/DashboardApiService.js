32a40b90941600169c2499c4c6b4f0dc7b670061
Fenne <fennehattink@gmail.com>
2022-11-21 15:29:33 +0100

    fixed graph route

--- frontend/src/Services/Dashboard/DashboardApiService.js
+++ frontend/src/Services/Dashboard/DashboardApiService.js
@@ -5,20 +5,5 @@ class DashboardApiService extends ApiAdapter {
     super("orderMonths");
     this.headers = AuthHeader();
   }
-
-  async findByMonth(orderMonths) {
-    return this.get(`/${this.resource}/${orderMonths}`)
-      .then((response) => {
-        return response.data;
-
-        //return response.data;
-      })
-      .catch((error) => {
-        // You can handle the error, like show a notificaiton to the user
-
-        // dont forget to re-throw the error, otherwise the promise will resolve successfully
-        throw error;
-      });
-  }
 }
 export default DashboardApiService;
