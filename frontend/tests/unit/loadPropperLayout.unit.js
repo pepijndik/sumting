b44f860305ecc43e0cefe10312e3edb19dc85d1e
Pepijn dik <pepijn@pdik.nl>
2022-12-07 11:25:47 +0100

    Frontend testing

---
+++ frontend/tests/unit/loadPropperLayout.unit.js
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue';
+import Login from '@/Pages/Auth/Login.vue';
+import Router from "@/Router/router";
+let wrapper;
+
+beforeEach(async function() {
+    // eslint-disable-next-line no-undef
+    wrapper = mount(App, {
+        global: {
+            plugins: [Router]
+        }
+    });
+    await Router.isReady();
+});
+describe('Loads guest layout', () => {
+    it('Login is shown', () => {
+        //const wrapper = shallowMount(Login)
+        expect(wrapper.find(Login).exists()).toBe(true);
+    })
+})
+
