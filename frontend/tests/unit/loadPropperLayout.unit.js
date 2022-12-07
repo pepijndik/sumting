b49007240fc875584aaf0426f0cc22be1d0ec6fb
Pepijn dik <pepijn@pdik.nl>
2022-12-07 12:26:17 +0100

    Fix test inject

--- frontend/tests/unit/loadPropperLayout.unit.js
+++ frontend/tests/unit/loadPropperLayout.unit.js
@@ -1,22 +1,35 @@
-import { mount } from '@vue/test-utils'
+import {mount, shallowMount} from '@vue/test-utils'
 import App from '@/App.vue';
 import Login from '@/Pages/Auth/Login.vue';
 import Router from "@/Router/router";
+import AuthenticationService from "@/Services/AuthenticationService";
+import {reactive} from "vue";
 let wrapper;
-
+let auth = new AuthenticationService();
 beforeEach(async function() {
     // eslint-disable-next-line no-undef
     wrapper = mount(App, {
         global: {
-            plugins: [Router]
+            plugins: [Router],
+            provide: {
+                Auth: reactive(auth),
+            }
         }
     });
+
     await Router.isReady();
 });
 describe('Loads guest layout', () => {
     it('Login is shown', () => {
-        const wrapper = shallowMount(Login)
-        expect(wrapper.exists()).toBe(true);
+        const wrapper = shallowMount(Login,{
+            global: {
+                provide: {
+                    Auth: reactive(auth),
+                }
+            }
+        })
+        const LoginComp = wrapper.findComponent({ name: 'login' })
+        expect(LoginComp.exists()).toBe(true)
     })
 })
 
