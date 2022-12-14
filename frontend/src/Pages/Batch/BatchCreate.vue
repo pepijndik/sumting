f83bd2f58fa6dd5b2d84bbd6c0c9387960b43a2b
Dia Fortmeier <dia.fortmeier@hva.nl>
2022-12-14 03:50:54 +0100

    Added project field to BatchCreate

--- frontend/src/Pages/Batch/BatchCreate.vue
+++ frontend/src/Pages/Batch/BatchCreate.vue
@@ -1,10 +1,42 @@
 <template>
-<div>test</div>
+  <div class="grid w-full grid-cols-1 lg:grid-cols-3 md:grid-cols-1 gap-1 mt-7">
+    <div>
+      <p class="font-inter text-yInMnBlue">Project</p>
+      <SearchableDropdown
+          class="mt-1"
+          placeholder="Choose a project"
+          :fields="['description', 'type.description']"
+          :text="['description']"
+          :primarykey="'id'"
+          :return="'object'"
+          @selected="selectedProject = $event"
+          :options="projects">
+      </SearchableDropdown>
+    </div>
+  </div>
+
+  <div>
+    <p v-if="selectedProject !== null" v-text="selectedProject.description_long"></p>
+  </div>
 </template>
 
 <script>
+import SearchableDropdown from "@/Components/Form/SearchableDropdown";
 export default {
-  name: "BatchCreate"
+  name: "BatchCreate",
+  components: {
+    SearchableDropdown
+  },
+  inject: ['ProjectApi'],
+  data() {
+    return {
+      projects: [],
+      selectedProject: null
+    }
+  },
+  async created() {
+    this.projects = await this.ProjectApi.SearchableDropDown();
+  }
 }
 </script>
 
