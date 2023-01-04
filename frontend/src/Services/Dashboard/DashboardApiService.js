e6f1c382c6ddd173587c81ab2f98344df952682e
Kaan <kaan.ugur@hva.nl>
2023-01-04 22:23:43 +0100

    changes

--- frontend/src/Services/Dashboard/DashboardApiService.js
+++ frontend/src/Services/Dashboard/DashboardApiService.js
@@ -1,17 +1,21 @@
 import ApiAdapter from "@/Services/ApiAdapter";
 import AuthHeader from "@/Services/AuthHeader";
 import axios from "axios";
+
+// const API_URL = process.env.VUE_APP_API_URL + '/api/test/';
+// const API_URL = process.env.VUE_APP_BACKEND_API_URL
+
 class DashboardApiService extends ApiAdapter {
   constructor() {
     super("orderMonths");
     this.headers = AuthHeader();
   }
 
-  getAllProjectDescriptions(){
-    return axios.get(this.resource + "/projectDescriptions", {
-      headers: AuthHeader()
-    })
-  }
+  // getAllProjectDescriptions(){
+  //   return axios.get(this.resource + "orderMonths/projectDescriptions", {
+  //     headers: AuthHeader()
+  //   })
+  // }
 
   // async getAllProjectDescriptions() {
   //   const Projects = await BaseApi.get(this.resource + "/projectDescriptions").then((response) => {
