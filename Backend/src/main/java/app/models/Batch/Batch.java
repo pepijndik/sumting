56e1ae50b1c3389a67b9364d94afce3686a02e9f
Pepijn dik <pepijn@pdik.nl>
2023-01-06 14:37:21 +0100

    Multiple  Jsonback ref, fixed

--- Backend/src/main/java/app/models/Batch/Batch.java
+++ Backend/src/main/java/app/models/Batch/Batch.java
@@ -66,7 +66,7 @@ public class Batch implements Identifiable<Integer> {
     @OneToMany(cascade = CascadeType.ALL)
     @JsonView(BatchView.Batch.class)
     @JoinColumn(name = "batch_key", referencedColumnName = "batch_key", insertable = false, updatable = true)
-    @JsonManagedReference
+    @JsonManagedReference(value = "batch_orderline")
     private List<OrderLine> orderLines;
     @Override
     public Integer getId() {
