fd7c9f27fc789687de61789444a6c54652e62ac7
Kaan <kaan.ugur@hva.nl>
2023-01-08 13:27:00 +0100

    finshed update test

--- Backend/src/test/java/app/rest/OrderControllerTest.java
+++ Backend/src/test/java/app/rest/OrderControllerTest.java
@@ -3,12 +3,12 @@ package app.rest;
 import app.models.Order.OrderLine;
 import app.repositories.DataLoader;
 import app.repositories.Order.OrderlineRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,14 +18,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,6 +40,7 @@ public class OrderControllerTest {
     @Mock
     private OrderLine orderLine;
     @InjectMocks
+    @Autowired
     private OrderController orderController;
     @Autowired
     CommandLineRunner dataLoader;
@@ -74,57 +73,89 @@ public class OrderControllerTest {
     /**
      * Author: Kaan Ugur
      * Description: Checks if "/orders" return an ok response
+     *
      * @throws Exception if response is not ok
      */
     @Test
     public void ResponseEntityReturnsOk() throws Exception {
         // UnknownContentTypeException
         mvc.perform(MockMvcRequestBuilders
-                        .get("/orders")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());
+                        .get("/orders") // Calls the /orders endpoint
+                        .accept(MediaType.APPLICATION_JSON)) // Only accepts Json as a response
+                .andDo(print()) // Print the result
+                .andExpect(status().isOk()); // Expects that the response is ok
     }
 
 
+    /**
+     * Author: Kaan Ugur
+     * Description: Updates an existing order line
+     */
     @Test
-    public void EditAnOrderLine(){
-        // test order line
-        OrderLine orderLineDummy = new OrderLine(1, "Notes", 22.0, "proof name", 45.0, 56.0, "proofSmall", "proofMedium",
+    public void EditAnOrderLine() {
+        // Created dummy data
+        OrderLine orderLineDummy = new OrderLine(155, "Notes", 22.0, "proof name"
+                , 45.0, 56.0, "proofSmall", "proofMedium",
                 "proofLarge", 13.0, 14.0, null);
         LocalDateTime updatedLoadedDate = LocalDateTime.of(2022, 2, 22, 14, 12);
 
-        OrderLine updated = new OrderLine(1,"This is an updated Note", 22.0,
-                "Proof name updated", 31.0, 32.0, "Small proof updated",
-                "Medium proof updated", "Large proof updated", 12.0,
-                12.0, updatedLoadedDate);
+        // Updated the dummy data
+        orderLineDummy.setLoadedDate(updatedLoadedDate);
+        orderLineDummy.setNotes("This is an updated note");
+        orderLineDummy.setProofName("This is an edited proof name");
+        orderLineDummy.setProofSmall("This is an edited small proof name");
+        orderLineDummy.setProofMedium("This is an edited medium proof name");
+        orderLineDummy.setProofLarge("This is an edited large proof name");
 
-//        OrderLine updateOrderLine = orderController.editOrder(orderLineDummy.getId(), requestBody);
+        // Saved the updated dummy data
+        OrderLine savedOrderLine = orderlineRepository.save(orderLineDummy);
 
-//        Book book = new Book(0, "Some fancy Book");
-//        book.setType(Book.Type.Hardcover);
-//        Book savedBook = booksRepo.save(book);
-//        // check id generation
-//        assertTrue(savedBook.getId() > 0);
-//        // check added attributes
-//        assertEquals("Some fancy Book", savedBook.getTitle());
-//        assertEquals(Book.Type.Hardcover, savedBook.getType());
-//
-//        // find added book
-//        Book newBook = booksRepo.findById(savedBook.getId());
-//        assertEquals(newBook.getId(), savedBook.getId());
-//        assertEquals(newBook.getTitle(), savedBook.getTitle());
-//        assertEquals(newBook.getType(), savedBook.getType());
-//
-//        // change the book
-//        savedBook.setTitle("Changed Book");
-//        newBook = booksRepo.save(savedBook);
-//        // check persistence of change
-//        assertEquals(newBook.getTitle(), savedBook.getTitle());
-//        newBook = booksRepo.findById(savedBook.getId());
-//        assertEquals(newBook.getTitle(), savedBook.getTitle());
+        // Checks if the loadedDate is updated by checking it if it is not null
+        assertNotNull(savedOrderLine.getLoadedDate());
 
+        // check updated attributes
+        Assertions.assertEquals("This is an updated note", savedOrderLine.getNotes());
+        Assertions.assertEquals("This is an edited proof name", savedOrderLine.getProofName());
+        Assertions.assertEquals("This is an edited small proof name", savedOrderLine.getProofSmall());
+        Assertions.assertEquals("This is an edited medium proof name", savedOrderLine.getProofMedium());
 
+        // Checks if the order line is actually changed
+        Assertions.assertNotEquals("proofLarge", savedOrderLine.getProofLarge());
     }
 
+    /**
+     * Author: Kaan Ugur
+     * Description: The test fails because the expected value is the old value and not the updated value
+     */
+    @Test
+    public void WrongInputWhileUpdate() {
+        // Created dummy data
+        OrderLine orderLineDummy = new OrderLine(155, "Notes", 22.0, "proof name"
+                , 45.0, 56.0, "proofSmall", "proofMedium",
+                "proofLarge", 13.0, 14.0, null);
+        LocalDateTime updatedLoadedDate = LocalDateTime.of(2022, 2, 22, 14, 12);
+
+        // Updated the dummy data
+        orderLineDummy.setLoadedDate(updatedLoadedDate);
+        orderLineDummy.setNotes("This is an updated note");
+        orderLineDummy.setProofName("This is an edited proof name");
+        orderLineDummy.setProofSmall("This is an edited small proof name");
+        orderLineDummy.setProofMedium("This is an edited medium proof name");
+        orderLineDummy.setProofLarge("This is an edited large proof name");
+
+        // Saved the updated dummy data
+        OrderLine savedOrderLine = orderlineRepository.save(orderLineDummy);
+
+        // Checks if the loadedDate is updated by checking it if it is not null
+        assertNotNull(savedOrderLine.getLoadedDate());
+
+        // check updated attributes
+        Assertions.assertEquals("This is an updated note", savedOrderLine.getNotes());
+        Assertions.assertEquals("This is an edited proof name", savedOrderLine.getProofName());
+        Assertions.assertEquals("This is an edited small proof name", savedOrderLine.getProofSmall());
+        Assertions.assertEquals("This is an edited medium proof name", savedOrderLine.getProofMedium());
+
+        // Checks if the order line is actually changed
+        Assertions.assertEquals("proofLarge", savedOrderLine.getProofLarge());
+    }
 }
