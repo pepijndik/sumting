304c9be06910dfc6519a02fda502784b55639d11
Fenne <fennehattink@gmail.com>
2023-01-12 10:40:49 +0100

    added missing tabs

--- frontend/src/Layouts/AdminLayout.vue
+++ frontend/src/Layouts/AdminLayout.vue
@@ -33,11 +33,31 @@
             />
           </NavigationDropdownItem>
           <NavigationDropdownItem name="Batch" source="file.svg">
-            <SubDropdownItem to="/batches" name="View batches" source="file.svg"/>
-            <SubDropdownItem to="/batch/create" name="Create batch" source="create.svg"/>
+            <SubDropdownItem
+              to="/batches"
+              name="View batches"
+              source="file.svg"
+            />
+            <SubDropdownItem
+              to="/batch/create"
+              name="Create batch"
+              source="create.svg"
+            />
           </NavigationDropdownItem>
           <NavigationDropdownItem name="Client" source="user.svg">
             <SubDropdownItem to="/clients" name="Client's" source="user.svg" />
+            <SubDropdownItem
+              to="/clients/create"
+              name="Create Client"
+              source="create.svg"
+            />
+          </NavigationDropdownItem>
+          <NavigationDropdownItem name="Project" source="file.svg">
+            <SubDropdownItem
+              to="/projects"
+              name="View projects"
+              source="file.svg"
+            />
           </NavigationDropdownItem>
         </template>
       </Navigation>
@@ -55,9 +75,9 @@
           </div>
           <div v-if="isLoading" class="absolute z-40 ml-80 mt-24">
             <semipolar-spinner
-                :animation-duration="2000"
-                :size="65"
-                color="#E56B6F"
+              :animation-duration="2000"
+              :size="65"
+              color="#E56B6F"
             />
           </div>
           <router-view />
@@ -73,12 +93,19 @@ import DashboardHeaderBar from "@/Layouts/Header/Admin/AdminHeaderBar";
 import NavigationItem from "@/Layouts/Navigation/Admin/NavigationItem";
 import NavigationDropdownItem from "@/Layouts/Navigation/Admin/NavigationDropdownItem";
 import SubDropdownItem from "@/Layouts/Navigation/Admin/SubDropdownItem";
-import { SemipolarSpinner  } from 'epic-spinners'
+import { SemipolarSpinner } from "epic-spinners";
 
 export default {
   name: "DashboardLayout",
   inject: ["Auth"],
-  components: {NavigationItem, NavigationDropdownItem, SubDropdownItem, DashboardHeaderBar, Navigation,SemipolarSpinner},
+  components: {
+    NavigationItem,
+    NavigationDropdownItem,
+    SubDropdownItem,
+    DashboardHeaderBar,
+    Navigation,
+    SemipolarSpinner,
+  },
   data() {
     return {
       mobileOpen: false,
@@ -86,7 +113,7 @@ export default {
       axiosInterceptor: null,
     };
   },
-}
+};
 </script>
 
 <style scoped></style>
