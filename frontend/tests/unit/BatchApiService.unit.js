04161b0b1d4532b63703a353170cc688828c9911
Dia Fortmeier <dia.fortmeier@hva.nl>
2023-01-17 19:31:04 +0100

    Added creates a batch test [FE]

--- frontend/tests/unit/BatchApiService.unit.js
+++ frontend/tests/unit/BatchApiService.unit.js
@@ -2,12 +2,15 @@ import BaseApi from '@/Services/BaseApi';
 import Batch from "@/Models/Batch";
 jest.mock('@/Services/BaseApi', () => {
     return {
-        get: jest.fn(() => Promise.resolve({data: mockBatches}))
+        get: jest.fn(() => Promise.resolve({data: mockBatches})),
+        post: jest.fn(() => Promise.resolve({data: mockBatch}))
     }
 });
 
 import BatchApiService from '@/Services/Batch/BatchApiService'
 import mockBatches from '../mockingData/Batch/listOfBatches.json'
+import mockBatch from '../mockingData/Batch/batch.json'
+import moment from "moment";
 
 describe('BatchApiService', () => {
     let batchApiService;
@@ -28,6 +31,35 @@ describe('BatchApiService', () => {
             expect(actualBatches).toEqual(expectedBatches);
             expect(BaseApi.get).toHaveBeenCalled();
             expect(BaseApi.get).toHaveBeenCalledWith(batchApiService.resource);
+        });
+    });
+
+    describe('create', () => {
+        it('creates a batch', async () => {
+            const expectedBatch = {
+                id: mockBatch.id,
+                textPlanned: mockBatch.textPlanned,
+                batchSize: mockBatch.batchSize,
+                projectKey: mockBatch.projectKey,
+                orderlines: mockBatch.orderlines,
+                createdAt: mockBatch.createdAt
+            };
+
+            const result = await batchApiService.create(
+                mockBatch.textPlanned,
+                mockBatch.batchSize,
+                mockBatch.projectKey,
+                mockBatch.orderlines
+            );
+
+            expect(result).toEqual(expectedBatch);
+            expect(BaseApi.post).toHaveBeenCalledWith('/batch', {
+                textPlanned: mockBatch.textPlanned,
+                batchSize: mockBatch.batchSize,
+                projectKey: mockBatch.projectKey,
+                orderlines: mockBatch.orderlines,
+                createdAt: moment().format("YYYY-MM-DD")
+            });
         });
     });
 });
